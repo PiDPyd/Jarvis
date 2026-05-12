2c2b05fddb6f6af510445c33f855b9def3b85f88
PiDPyd Dev <piyush.element@gmail.com>
2026-05-12 19:30:53 +0530

    jarvis can now actively listen to my commands the faster-whisper tiny model is proving to be fast but sometimes its unreliable, future changes to the speech to text system might happen.

--- jarvis_main.py
+++ jarvis_main.py
@@ -2,19 +2,61 @@ import openwakeword
 from openwakeword.model import Model
 
 import pyaudio
+import wave
 import numpy as np
+import time
+from faster_whisper import WhisperModel
 
-model = Model(wakeword_models=["C:\\Users\\piyus\\AppData\\Local\\Programs\\Python\\Python38\\lib\\site-packages\\openwakeword\\resources/models/hey_jarvis_v0.1.tflite"])
+
+model = Model(wakeword_models=["C:\\Users\\piyus\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\site-packages\\openwakeword\\resources\\models\\hey_jarvis_v0.1.onnx"],inference_framework="onnx")
 microphone = pyaudio.PyAudio()
+whisper_model = WhisperModel("tiny", device="cpu", compute_type="int8")
 
 audiofeed = microphone.open(rate=16000, channels=1,format=pyaudio.paInt16,frames_per_buffer=1280,input=True)
-print("Jarvis is listening...")
+
+print("jarvis is online")
+
 while True:
     rawfeed = audiofeed.read(1280, exception_on_overflow=False)
     wakefeed = np.frombuffer(rawfeed, dtype=np.int16)
     prediction = model.predict(wakefeed)
     if prediction["hey_jarvis_v0.1"] > 0.7:
-        print("Detected.")
+        model.reset()
+        print("jarvis is awakened. actively Listening...")
+        frames = []
+        RATE = 16000
+        CHUNK = 1280
+        SILENCE_THRESHOLD = 500
+        SILENCE_DURATION = 1.5
+        silent_chunks = 0
+        max_silent_chunks = int(RATE / CHUNK * SILENCE_DURATION)
+        while True:
+            data = audiofeed.read(CHUNK, exception_on_overflow=False)
+            frames.append(data)
+            audio_chunk = np.frombuffer(data, dtype=np.int16)
+            volume = np.abs(audio_chunk).mean()
+            if volume < SILENCE_THRESHOLD:
+                silent_chunks +=1 
+            else:
+                silent_chunks = 0
+
+            if silent_chunks >= max_silent_chunks:
+                print("Silence detected.")
+                break
+
+        with wave.open("command.wav", "wb") as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)
+            wf.setframerate(RATE)
+            wf.writeframes(b"".join(frames))
+
+        t1 = time.time()
+        segments, _ = whisper_model.transcribe("command.wav", language="en")
+        command = " ".join([s.text for s in segments])
+        t2 = time.time()
+        print(f"Transcribe took: {t2-t1:.2f}s")
+        print(command)
+
 
 
 
